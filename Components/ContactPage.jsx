75a1d6df542e6e0ddc326834c418f7e365311399
Chris Woodby <chriswoodby@Chriss-MacBook-Pro.local>
2022-01-29 16:23:35 -0600

    cleaned up about & contact components

--- Components/ContactPage.jsx
+++ Components/ContactPage.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 
 const ContactPage = function() {
   return (
@@ -9,14 +7,12 @@ const ContactPage = function() {
       <h2 id="contact-title">lets get in touch</h2>
       <p id="contact-details">please feel free to email us anytime!</p>
       <p id="contact-details-bottom">phone hours: 9am - 7pm</p>
-          {/* <Col></Col> */}
-        <div id="contact-text-bubble">
-          <h3>Contact via Email</h3>
-          <p>transitmanagmentconsulting@gmail.com</p>
-          <h3>Give us a Call</h3>
-          <p>(512) 626-7681</p>
-        </div>
-        {/* <Col></Col> */}
+      <div id="contact-text-bubble">
+        <h3>Contact via Email</h3>
+        <p>transitmanagmentconsulting@gmail.com</p>
+        <h3>Give us a Call</h3>
+        <p>(512) 626-7681</p>
+      </div>
     </Container>
   )
 }
